dd21cd61e50fa68ea07292a746d4e913a9aad60f
Artsem Samonau <artemgomel89@gmail.com>
2020-09-16 20:49:09 +0300

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let newString = (n + "").split("");
+    if (newString[0] === "-") {
+        return newString.slice(1).reverse().join("") * 1;
+    }
+    return newString.reverse().join("") * 1;
+};
